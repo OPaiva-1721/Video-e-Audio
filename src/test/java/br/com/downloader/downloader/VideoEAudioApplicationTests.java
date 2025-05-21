c9201523c96961c1376864f5ebc0e61991fd385d
OPaiva-1721 <gabryelpaiva123@gmail.com>
2025-05-21 01:36:59 -0300

    Fix/all bug for deploy

--- src/test/java/br/com/downloader/downloader/VideoEAudioApplicationTests.java
+++ src/test/java/br/com/downloader/downloader/VideoEAudioApplicationTests.java
@@ -1,7 +1,6 @@
 package br.com.downloader.downloader;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
 @SpringBootTest
 class VideoEAudioApplicationTests {
