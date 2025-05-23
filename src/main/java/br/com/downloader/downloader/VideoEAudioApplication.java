42aef07a0b37c5f5d41e20d85c09d6a169f5be3a
OPaiva-1721 <gabryelpaiva123@gmail.com>
2025-05-23 19:31:29 -0300

    Update VideoEAudioApplication.java

--- src/main/java/br/com/downloader/downloader/VideoEAudioApplication.java
+++ src/main/java/br/com/downloader/downloader/VideoEAudioApplication.java
@@ -2,6 +2,7 @@ package br.com.downloader.downloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
